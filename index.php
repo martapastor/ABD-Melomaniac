cf37caac684917a9a9967df6d265ddef54143e83
Marta Pastor Puente <marta.pastor@live.com>
2017-05-10 19:04:28 +0200

    Added main page that redirects to login or register form

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+
+        <!-- jQuery library -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <!-- Bootstrap library -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Google fonts -->
+		<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Abel'>
+		<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Actor'>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="js/scripts.js"></script>
+
+        <link rel="icon" type="image/ico" href="img/favicon.png">
+        <title>Melomaniac</title>
+    </head>
+
+    <body>
+        <div class = "container-fluid">
+            <div class = "row">
+                <img class = "logo" alt="Melomaniac Logo" src="img/logo.png">
+            </div>
+            <div class = "row">
+                <br>
+                <br>
+                <br>
+                <br>
+            </div>
+            <div class = "row">
+                <div class = "col-md-4 index-button" href="src/login_form.php">
+                    <a class = "index-button-text" href="src/login_form.php">Login</a>
+                </div>
+                <div class = "col-md-4 index-button">
+                    <a class = "index-button-text" href="src/signup_form.php">Register</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
